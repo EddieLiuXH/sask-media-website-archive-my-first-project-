5ec3887fe0a0263e820f36ee4c160db6cfa648cb
EddieLiuXH <112829579+EddieLiuXH@users.noreply.github.com>
2024-03-18 15:27:18 -0600

    Add files via upload

---
+++ SaskMedia Project - Copy/script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // This is where you can add JavaScript interactions or functionality
+    console.log("Document loaded. Add your JS here.");
+});
